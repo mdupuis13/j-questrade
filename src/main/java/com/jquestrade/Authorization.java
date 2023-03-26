db1d4b46b72ca897ff467dd2fad1524ba8d01caf
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-03-25 22:59:40 -0400

    Modernize code to java 17 standards
    Extract an interface to ease dependency injection and testing
    Add lombok to Authorization class
    Use record for Account class

--- src/main/java/com/jquestrade/Authorization.java
+++ src/main/java/com/jquestrade/Authorization.java
@@ -1,6 +1,9 @@
 package com.jquestrade;
 
+import lombok.Getter;
+
 /** Represents the data that allows an application to make requests the Questrade API. */
+@Getter
 public class Authorization {
 	
 	/** Creates a Authorization object using cached data. */
@@ -12,48 +15,32 @@ public class Authorization {
 		this.token_type = "Bearer"; // Access token type is always Bearer
 	}
 
-	private String access_token;
-	private String api_server;
-	private int expires_in;
-	private String refresh_token;
-	private String token_type;
-	
-	/** Returns the access token. Expires in 30 minutes after generation.
-	 * Only works with the assocatied API server, see {@link #getApiServer()}
-	 * @return The access token.
+	/**
+	 * The access token. Expires in 30 minutes after generation.
+	 * Only works with the associated API server, see {@link #api_server}
 	 */
-	public String getAccessToken() {
-		return access_token;
-	}
-	
-	/** Returns the URL of the API server assigned to the access token.
-	 * @return The URL of the API server assigned to the access token.
+	private final String access_token;
+
+	/**
+	 * The URL of the API server assigned to the access token.
 	 */
-	public String getApiServer() {
-		return api_server;
-	}
-	
-	/** Returns how long after generation the access token will expire.
-	 * @return How long after generation the access token will expire.
+	private final String api_server;
+
+	/**
+	 * Returns how long after generation the access token will expire.
 	 * It appears to always be {@code 1800} seconds (30 minutes).
 	 */
-	public int getAccessTokenExpiry() {
-		return expires_in;
-	}
-	
-	/** Returns the new refresh token, which can be used to generate a new {@code Authorization}.
-	 * @return The new refresh token.
+	private final int expires_in;
+
+	/**
+	 * The new refresh token, which can be used to generate a new {@code Authorization}.
 	 */
-	public String getRefreshToken() {
-		return refresh_token;
-	}
-	
-	/** The access token type.
-	 * @return The access token type. Is always <b>Bearer</b>
+	private final String refresh_token;
+
+	/**
+	 * The access token type. Is always <b>Bearer</b>
 	 */
-	public String getTokenType() {
-		return token_type;
-	}
-	
-	
+	private final String token_type;
+
+
 }
