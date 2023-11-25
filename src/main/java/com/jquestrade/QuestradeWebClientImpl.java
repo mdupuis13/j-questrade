c0c1abdc72126dbb6c28fc7a9b8f42987debe3cb
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-11-25 12:29:45 -0500

    Refactor auth test

--- src/main/java/com/jquestrade/QuestradeWebClientImpl.java
+++ src/main/java/com/jquestrade/QuestradeWebClientImpl.java
@@ -2,7 +2,22 @@ package com.jquestrade;
 
 public class QuestradeWebClientImpl implements QuestradeWebClient {
 
-    public QuestradeWebClientImpl(String refreshToken, String accessToken, String apiServerURL) {
+    private String refreshToken;
+    private String accessToken;
+    private String apiServerURL;
 
+    private final String QUESTRADE_URL ="https://login.questrade.com";
+    private final String AUTH_URI = QUESTRADE_URL + "/oauth2/token?grant_type=refresh_token&refresh_token=%s";
+
+    public QuestradeWebClientImpl() {
+    }
+
+    @Override
+    public Authorization authenticate(String refreshToken, String accessToken, String apiServerURL) {
+        this.accessToken = accessToken;
+        this.apiServerURL = apiServerURL;
+        this.refreshToken = refreshToken;
+
+        return new Authorization(refreshToken,accessToken,apiServerURL);
     }
 }
