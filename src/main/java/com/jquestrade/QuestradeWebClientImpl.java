066e3bc870e151682bc6e18f951ab2aa7500cfee
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2023-11-25 08:40:13 -0500

    Create new questrade client with Springboot WebClient

---
+++ src/main/java/com/jquestrade/QuestradeWebClientImpl.java
@@ -0,0 +1,8 @@
+package com.jquestrade;
+
+public class QuestradeWebClientImpl implements QuestradeWebClient {
+
+    public QuestradeWebClientImpl(String refreshToken, String accessToken, String apiServerURL) {
+
+    }
+}
