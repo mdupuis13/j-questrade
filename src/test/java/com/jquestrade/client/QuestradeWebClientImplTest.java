60e2876e5e1922209d25e0abe7ff873a3c7cf0db
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-02 00:06:30 -0500

    Add Junit Pioneer lib + tests

--- src/test/java/com/jquestrade/client/QuestradeWebClientImplTest.java
+++ src/test/java/com/jquestrade/client/QuestradeWebClientImplTest.java
@@ -3,6 +3,7 @@ package com.jquestrade.client;
 import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import com.jquestrade.AuthenticationToken;
 import com.jquestrade.client.config.WebClientProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.instancio.Instancio;
 import org.instancio.junit.InstancioExtension;
 import org.junit.jupiter.api.AfterEach;
@@ -10,16 +11,20 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junitpioneer.jupiter.DefaultTimeZone;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.OffsetDateTime;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(InstancioExtension.class)
 @ExtendWith(MockitoExtension.class)
+@Slf4j
 class QuestradeWebClientImplTest {
 
     @RegisterExtension
@@ -62,5 +67,17 @@ class QuestradeWebClientImplTest {
         AuthenticationToken token = sut.authenticate(oldRefreshToken);
 
         assertThat(token.isValid()).isTrue();
+    }
+
+    @Test
+    @DefaultTimeZone("PST")
+    void givenIAmAuthenticated_WhenReadTheExpirationDate_ItIsInMyTimeZone() {
+        // change JVM timezone for this specific test to Pacific Time
+        // the date should be between 2 and 3 hours later than the wiremock
+        String oldRefreshToken = Instancio.create(String.class);
+
+        AuthenticationToken token = sut.authenticate(oldRefreshToken);
+
+        assertThat(token.expires_at()).isAfterOrEqualTo(OffsetDateTime.now());
     }
 }
