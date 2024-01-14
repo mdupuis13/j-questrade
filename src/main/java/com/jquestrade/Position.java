54a0360d1c3f186eb87673a9622dca2e3d594bc8
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-14 14:27:58 -0500

    Creatge method to read the list of positions detained in an account (#9)
    * convert Position to java record
    
    * Add fct to get positions with tests
    
    * Rename Authorization class to betgter represent its use
    
    * Change logback logging level

--- src/main/java/com/jquestrade/Position.java
+++ src/main/java/com/jquestrade/Position.java
@@ -1,110 +1,33 @@
 package com.jquestrade;
 
-/** Represents a position in your Questrade account. 
+/**
+ * Represents a position in your Questrade account.
+ *
+ * @param symbol             Returns the symbol of the position.
+ * @param symbolId           Returns the internal symbol identifier.
+ * @param openQuantity       Returns the position quantity remaining open.
+ * @param closedQuantity     Returns the portion of the position that was closed today.
+ * @param currentMarketValue Returns the current market value of the position (quantity x price).
+ * @param currentPrice       Returns the current price of the position symbol.
+ * @param averageEntryPrice  Returns the average price paid for all executions constituting the position.
+ * @param closedPnl          Return The realized profit/loss on this position.
+ * @param openPnl            Return the total unrealized profit/loss on this position.
+ * @param totalCost          Returns the total cost of the position.
+ * @param isRealTime         Returns whether real-time quote was used to compute the profit-and-losses.
+ * @param isUnderReorg       Whether the symbol is currently undergoing a reorg.
  * @see <a href="https://www.questrade.com/api/documentation/rest-operations/account-calls/accounts-id-positions">
- * The position properties documentation</a>
+ * The positions documentation</a>
  */
-public class Position {
-	private String symbol;
-	private int symbolId;
-	private int openQuantity;
-	private int closedQuantity;
-	private double currentMarketValue;
-	private double currentPrice;
-	private double averageEntryPrice;
-	private double dayPnl;
-	private double closedPnl;
-	private double openPnl;
-	private double totalCost;
-	private boolean isRealTime;
-	private boolean isUnderReorg;
-	
-	
-	/** Returns the symbol of the position.
-	 * @return The symbol.
-	 */
-	public String getSymbol() {
-		return symbol;
-	}
-	
-	/** Returns the internal symbol identifier.
-	 * @return The internal symbol identifier.
-	 */
-	public int getSymbolId() {
-		return symbolId;
-	}
-	
-	/** Returns the position quantity remaining open.
-	 * @return The position quantity that is remaining open.
-	 */
-	public int getOpenQuantity() {
-		return openQuantity;
-	}
-	
-	/** Returns the portion of the position that was closed today.
-	 * @return The portion of the position that was closed today.
-	 */
-	public int getClosedQuantity() {
-		return closedQuantity;
-	}
-	
-	/** Returns the current market value of the position (quantity x price).
-	 * @return The current market value of the position.
-	 */
-	public double getCurrentMarketValue() {
-		return currentMarketValue;
-	}
-	
-	/** Returns the current price of the position symbol.
-	 * @return The current price of the position symbol.
-	 */
-	public double getCurrentPrice() {
-		return currentPrice;
-	}
-	
-	/** Returns the average price paid for all executions constituting the position.
-	 * @return The average price for this position.
-	 */
-	public double getAverageEntryPrice() {
-		return averageEntryPrice;
-	}
-	
-	/** Returns the profit-and-loss on this position for today.
-	 * @return the profit-and-loss for today.
-	 */
-	public double getDayPnl() {
-		return dayPnl;
-	}
-	
-	/** Return The realized profit/loss on this position.
-	 * @return The realized profit/loss on this position.
-	 */
-	public double getClosedPnl() {
-		return closedPnl;
-	}
-	
-	/** Return the total unrealized profit/loss on this position.
-	 * @return The total unrealized profit/loss on this position.
-	 */
-	public double getOpenPnl() {
-		return openPnl;
-	}
-	/** Returns the total cost of the position.
-	 * @return The total cost of the position.
-	 */
-	public double getTotalCost() {
-		return totalCost;
-	}
-	/** Returns whether real-time quote was used to compute the profit-and-losses.
-	 * @return Whether real-time quote was used to compute the profit-and-losses.
-	 */
-	public boolean isRealTime() {
-		return isRealTime;
-	}
-	/** Whether the symbol is currently undergoing a reorg.
-	 * @return Whether the symbol is currently undergoing a reorg.
-	 */
-	public boolean isUnderReorg() {
-		return isUnderReorg;
-	}
-}
+public record Position(
+        String symbol,
+        String symbolId,
+        int openQuantity,
+        int closedQuantity,
+        double currentMarketValue,
+        double currentPrice,
+        double averageEntryPrice,
+        double closedPnl,
+        double openPnl,
+        double totalCost,
+        boolean isRealTime,
+        boolean isUnderReorg) {}
--- src/main/java/com/jquestrade/client/QuestradeWebClientImpl.java
+++ src/main/java/com/jquestrade/client/QuestradeWebClientImpl.java
@@ -2,6 +2,7 @@ package com.jquestrade.client;
 
 import com.jquestrade.Account;
 import com.jquestrade.AuthenticationToken;
+import com.jquestrade.Position;
 import com.jquestrade.client.config.WebClientProperties;
 import com.jquestrade.exceptions.AuthenticationException;
 import lombok.extern.slf4j.Slf4j;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
 
 import java.time.OffsetDateTime;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
@@ -30,13 +32,13 @@ public class QuestradeWebClientImpl implements QuestradeWebClient {
     public AuthenticationToken authenticate(String refreshToken) {
         log.info("QuestradeWebClient: Calling Questrade API with refresh token: {}", refreshToken);
 
-        ResponseEntity<Authorization> response = authenticationClient.get()
-                                                                     .uri(uriBuilder -> uriBuilder.path("/oauth2/token")
+        ResponseEntity<AuthorizationResponse> response = authenticationClient.get()
+                                                                             .uri(uriBuilder -> uriBuilder.path("/oauth2/token")
                                                                                                   .queryParam("grant_type", "refresh_token")
                                                                                                   .queryParam("refresh_token", refreshToken)
                                                                                                   .build())
-                                                                     .retrieve()
-                                                                     .toEntity(Authorization.class);
+                                                                             .retrieve()
+                                                                             .toEntity(AuthorizationResponse.class);
 
         return createAuthenticationObject(response);
     }
@@ -44,23 +46,32 @@ public class QuestradeWebClientImpl implements QuestradeWebClient {
     @Override
     public List<Account> getAccounts(AuthenticationToken authToken) {
         log.info("QuestradeWebClient: Calling Questrade API getAccounts()");
-        ResponseEntity<AccountResponse> response = callQuestrade(authToken, "accounts");
+        ResponseEntity<AccountResponse> response =
+                callQuestrade(authToken, "accounts").toEntity(AccountResponse.class);
 
-        return response.getBody().accounts();
+        return response.getBody() == null ? Collections.emptyList() : response.getBody().accounts();
     }
 
-    private ResponseEntity<AccountResponse> callQuestrade(AuthenticationToken authToken, String ressource) {
+    @Override
+    public List<Position> getPositions(AuthenticationToken authToken, Account account) {
+        log.info("QuestradeWebClient: Calling Questrade API getPositions()");
+        ResponseEntity<PositionsResponse> response =
+                callQuestrade(authToken, "accounts/%s/positions".formatted(account.number())).toEntity(PositionsResponse.class);
+
+        return response.getBody() == null ? Collections.emptyList() : response.getBody().positions();
+    }
+
+    private RestClient.ResponseSpec callQuestrade(AuthenticationToken authToken, String ressource) {
 
         return apiClient.get()
                         .uri(API_V1_TEMPLATE.formatted(authToken.api_server(), ressource))
                         .header("Authorization", "Bearer %s".formatted(authToken.access_token()))
-                        .retrieve()
-                        .toEntity(AccountResponse.class);
+                        .retrieve();
     }
 
-    private AuthenticationToken createAuthenticationObject(ResponseEntity<Authorization> response) {
+    private AuthenticationToken createAuthenticationObject(ResponseEntity<AuthorizationResponse> response) {
         log.info("QuestradeWebClient: Creating authentication object from API answer");
-        Authorization clientAuth = response.getBody();
+        AuthorizationResponse clientAuth = response.getBody();
 
         if (clientAuth == null) throw new AuthenticationException("Cannot retrieve auth token");
 
@@ -70,7 +81,7 @@ public class QuestradeWebClientImpl implements QuestradeWebClient {
         return new AuthenticationToken(clientAuth.access_token(), clientAuth.api_server(), expiresAt, clientAuth.refresh_token(), clientAuth.token_type());
     }
 
-    private OffsetDateTime getExpirationDate(String dateHeader, Authorization clientAuth) {
+    private OffsetDateTime getExpirationDate(String dateHeader, AuthorizationResponse clientAuth) {
         OffsetDateTime localDate = DateUtils.parseHeaderDateToLocalOffsetDateTime(dateHeader);
 
         return localDate.plusSeconds(clientAuth.expires_in());
