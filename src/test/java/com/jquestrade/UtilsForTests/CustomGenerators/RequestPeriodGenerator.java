0fb99669f68b2aa52635500a0c41041353253f45
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-14 21:25:10 -0500

    Cleanup

--- src/test/java/com/jquestrade/UtilsForTests/CustomGenerators/RequestPeriodGenerator.java
+++ src/test/java/com/jquestrade/UtilsForTests/CustomGenerators/RequestPeriodGenerator.java
@@ -5,15 +5,11 @@ import org.instancio.Instancio;
 import org.instancio.Random;
 import org.instancio.generator.Generator;
 
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
 
 public class RequestPeriodGenerator implements Generator<RequestPeriod> {
     public static final int MAX_DAYS_IN_PERIOD = 30;
-    public static final int NB_LEGAL_YEARS_TO_KEEP = 7;
-    private static final LocalDateTime LAST_POSSIBLE_DATE = LocalDateTime.now().minusDays(1);
-    private static final LocalDateTime FIRST_POSSIBLE_DATE = LAST_POSSIBLE_DATE.minusYears(NB_LEGAL_YEARS_TO_KEEP);
 
     @Override
     public RequestPeriod generate(Random random) {
