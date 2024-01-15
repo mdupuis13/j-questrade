f2f907dd554e053fd011af76cd4a7d17bbf49d6b
Martin Dupuis <4422549+mdupuis13@users.noreply.github.com>
2024-01-14 21:24:05 -0500

    Create getCandles() function

--- src/main/java/com/jquestrade/client/RequestPeriod.java
+++ src/main/java/com/jquestrade/client/RequestPeriod.java
@@ -2,10 +2,16 @@ package com.jquestrade.client;
 
 import java.time.OffsetDateTime;
 
+/**
+ * Represent a request period
+ *
+ * @param periodStart   Start date of the period
+ * @param periodEnd End date of the period, must happen after {@param periodStart}
+ */
 public record RequestPeriod (OffsetDateTime periodStart,
                              OffsetDateTime periodEnd) {
     public RequestPeriod {
-        if (periodStart.isAfter(periodEnd))
+        if (periodEnd.isBefore(periodStart))
             throw new IllegalArgumentException("Start date (%s) must be before or equal to End date (%s).".formatted(periodStart,periodEnd));
     }
 }
