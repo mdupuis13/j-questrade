1d44a185ade0315bffe80d8714deb228a2a6a97b
mdupuis <mdupuis13@gmail.com>
2024-05-25 16:05:02 -0400

    Fix auth token test

--- src/main/java/info/martindupuis/jquestrade/client/DateUtils.java
+++ src/main/java/info/martindupuis/jquestrade/client/DateUtils.java
@@ -9,13 +9,19 @@ import java.util.Locale;
 
 @Slf4j
 class DateUtils {
-    static final DateTimeFormatter DATE_HEADER_FORMAT = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss z", Locale.US);
+    static final DateTimeFormatter DATE_HEADER_FORMAT = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss zzz",
+            Locale.US);
 
     public static ZonedDateTime parseHeaderDateToLocalOffsetDateTime(String dateReceived) {
         ZonedDateTime dateParsed = ZonedDateTime.parse(dateReceived, DATE_HEADER_FORMAT);
         ZonedDateTime localDate = dateParsed.withZoneSameInstant(ZoneId.systemDefault());
 
-        log.info("QuestradeWebClient: Date received from header: '{}'    local expiration date: {}", dateReceived, localDate);
+        log.debug(
+                "QuestradeWebClient: ZoneID is {}   Date received from header: '{}'  date parsed: {}  local expiration date: {}",
+                ZoneId.systemDefault(),
+                dateReceived,
+                dateParsed,
+                localDate);
         return localDate;
     }
 }
